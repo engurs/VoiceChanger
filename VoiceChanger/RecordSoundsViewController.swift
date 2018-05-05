35ac33b90661d0fc81c2bf8217427c951a9b11dc
seznn <sezin.engur@stu.bahcesehir.edu.tr>
2018-05-05 16:39:55 +0300

    Added NSMicrophoneUsageDescription

---
+++ VoiceChanger/RecordSoundsViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  VoiceChanger
+//
+//  Created by Sezin Engür on 4/19/18.
+//  Copyright © 2018 Sez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        
+        recordingLabel.text = "Recording in Progress"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        recordingLabel.text = "Tap to Record"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
